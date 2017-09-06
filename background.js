232923e6faca58e1dfe210a1a39f17610b6b87aa
Benjamin Joldersma <ben@themaven.net>
2017-09-06 01:38:56 -0700

    analyze reviews as well as comments

---
+++ background.js
@@ -0,0 +1,126 @@
+let accessToken = ''
+let gitLogin = ''
+let githubOrg = ''
+const api = url => `https://api.github.com/${url}`
+const call = (url, qs) => fetch(`${url}?access_token=${accessToken}${qs ? '&' + qs : ''}`)
+  .then(r => r.json())
+
+const getRepos = () => call(api(`orgs/${githubOrg}/repos`))
+
+const getPulls = repoName => call(api(`repos/${githubOrg}/${repoName}/pulls`))
+
+const getReviews = pull => call(`${pull.url}/reviews`)
+
+const getComments = pull => call(`${pull.comments_url}`,
+  'per_page=50&sort=created&direction=desc')
+
+let data = {
+  repos: [],
+  inbox: [],
+  outbox: [],
+}
+
+const loadData = () => {
+  data = {
+    repos: [],
+    inbox: [],
+    outbox: [],
+  }
+  chrome.storage.sync.get({
+    login: '',
+    accessToken: '',
+    githubOrg: ''
+  }, function(config) {
+    accessToken = config.accessToken
+    gitLogin = config.login
+    githubOrg = config.githubOrg
+    getRepos().then(repos => {
+      for (let repo of repos) {
+        data.repos.push(repo)
+        getPulls(repo.name).then(pulls => {
+          repo.pulls = pulls
+          pulls.forEach(routePull)
+        })
+      }
+    })
+  })
+}
+
+const OPEN = 'OPEN'
+const CHANGES_REQUESTED = 'CHANGES_REQUESTED'
+const APPROVED = 'APPROVED'
+
+const routePull = pull => {
+  if (pull.requested_reviewers.some(r => r.login === gitLogin)) {
+    // I am a reviewer on this PR. I *think* we may be able to just call it here and put it in
+    // your inbox, since you disappear from requested_reviewers once you submit a review.
+    data.inbox.push(pull)
+    return
+  }
+  const isMyPull = pull.user.login === gitLogin
+  getReviews(pull).then(reviews => {
+    console.log(pull.url, 'reviews', reviews)
+    getComments(pull).then(comments => {
+      console.log(pull.url, 'comments', comments)
+      let entries = [...reviews, ...comments]
+        .sort((a, b) => new Date(a.submitted_at || a.updated_at) >
+          new Date(b.submitted_at || b.updated_at))
+      console.log(pull.url, 'entries', entries)
+      let myBall = false, theirBall = false
+      let currentState = OPEN
+      let iAmOnPull = false
+      for(const entry of entries) {
+        const isMyEntry = entry.user.login === gitLogin
+        if (entry.state === CHANGES_REQUESTED) {
+          iAmOnPull |= isMyEntry
+          myBall = isMyPull
+          theirBall = isMyEntry
+          currentState = CHANGES_REQUESTED
+        } else if (entry.state === APPROVED) {
+          iAmOnPull |= isMyEntry
+          myBall = isMyPull
+          theirBall = isMyEntry
+          currentState = APPROVED
+        } else if (entry.body.toLowerCase() === 'ptal') {
+          iAmOnPull |= isMyEntry
+          myBall = iAmOnPull && !isMyEntry
+          theirBall = isMyEntry
+        }
+      }
+      if (myBall) {
+        data.inbox.push(pull)
+        update()
+      } else if (theirBall) {
+        data.outbox.push(pull)
+        update()
+      }
+    })
+  })
+}
+
+const update = () => {
+  chrome.storage.local.set({ data })
+  if (chrome.browserAction) {
+    chrome.browserAction.setBadgeText({text: data.inbox.length ? '' + data.inbox.length : null})
+  }
+  chrome.runtime.sendMessage({cmd: 'render', data})
+}
+
+chrome.runtime.onStartup.addListener(() => {
+  chrome.alarms.create('check', {periodInMinutes: 5});
+  loadData()
+})
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.alarms.create('check', {periodInMinutes: 5});
+  loadData()
+})
+
+chrome.alarms.onAlarm.addListener(() => {
+  loadData()
+})
+
+chrome.runtime.onMessage.addListener(request => {
+  if (request.cmd == "loadData")
+    loadData()
+});
--- popup.js
+++ popup.js
@@ -2,26 +2,6 @@ function renderStatus(statusText) {
   document.getElementById('status').textContent = statusText
 }
 
-let accessToken = ''
-let gitLogin = ''
-let githubOrg = ''
-const api = url => `https://api.github.com/${url}`
-const call = (url, qs) => fetch(`${url}?access_token=${accessToken}${qs ? '&' + qs : ''}`)
-  .then(r => r.json())
-
-const getRepos = () => call(api(`orgs/${githubOrg}/repos`))
-
-const getPulls = repoName => call(api(`repos/${githubOrg}/${repoName}/pulls`))
-
-const getComments = pull => call(`${pull.review_comments_url}`,
-  'per_page=50&sort=created&direction=desc')
-
-const data = {
-  repos: [],
-  inbox: [],
-  outbox: [],
-}
-
 const renderPull = pull => `<div class="pr">
   <header>${pull.title}</header>
   <a href="${pull.html_url}">
@@ -29,7 +9,7 @@ const renderPull = pull => `<div class="pr">
   </a>
 </div>`
 
-const render = () => {
+const render = data => {
   const inbox = document.getElementById('inbox')
   const outbox = document.getElementById('outbox')
   const inboxHTML = '<h1>Inbox</h1>' + data.inbox.map(renderPull).join('\n')
@@ -38,52 +18,14 @@ const render = () => {
   outbox.innerHTML = outboxHTML
 }
 
-const mostRecentPtal = comments => comments.find(c => c.body.toLowerCase().includes('ptal'))
-
-const routePull = pull => {
-  if (pull.user.login === gitLogin) {
-    // I started this PR.
-    getComments(pull).then(comments => {
-      const ptal = mostRecentPtal(comments)
-      if (!ptal || ptal.user.login === gitLogin) {
-        data.outbox.push(pull)
-      } else {
-        data.inbox.push(pull)
-      }
-      render()
-    })
-  } else if (pull.requested_reviewers.some(r => r.login === gitLogin)) {
-    // I am a reviewer on this PR.
-    getComments(pull).then(comments => {
-      let ptal = mostRecentPtal(comments)
-      if (!ptal || ptal.user.login !== gitLogin) {
-        data.inbox.push(pull)
-      } else {
-        data.outbox.push(pull)
-      }
-      render()
-    })
-  }
-}
-
 document.addEventListener('DOMContentLoaded', function() {
-  chrome.storage.sync.get({
-    login: '',
-    accessToken: '',
-    githubOrg: ''
-  }, function(config) {
-    accessToken = config.accessToken
-    gitLogin = config.login
-    githubOrg = config.githubOrg
-    getRepos().then(repos => {
-      for (let repo of repos) {
-        data.repos.push(repo)
-        getPulls(repo.name).then(pulls => {
-          repo.pulls = pulls
-          pulls.forEach(routePull)
-        })
-      }
-    })
+  chrome.storage.local.get({
+    data: null
+  }, function({data}) {
+    if (data) {
+      render(data)
+    }
+    chrome.runtime.sendMessage({cmd: 'loadData'})
   })
 })
 
@@ -92,3 +34,8 @@ document.addEventListener('click', e => {
     chrome.tabs.create({url: e.target.href})
   }
 })
+
+chrome.runtime.onMessage.addListener(request => {
+  if (request.cmd == "render")
+    render(request.data)
+});
